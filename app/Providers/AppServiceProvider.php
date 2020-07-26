0a77c3942e775ae63193626754e20bc9ea74b1dc
Keigo Victor Fujita <keigofujita19@gmail.com>
2020-07-26 19:25:57 +0800

    Fixed the AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -27,14 +27,5 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        try {
-            $total_earnings = Call::select('amount_earned')
-                ->where('submitted_at', '>=', Carbon::parse('last monday'))
-                ->get()
-                ->sum('amount_earned');
-            view()->share('total_earnings', $total_earnings);
-        } catch (Exception $e) {
-            Log::error($e);
-        }
     }
 }

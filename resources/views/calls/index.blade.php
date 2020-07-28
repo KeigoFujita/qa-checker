00d6bb0efd8222602560ce0f0ce2c5a6acd2ea8f
Keigo Victor Fujita <keigofujita19@gmail.com>
2020-07-29 05:54:51 +0800

    Refactored alerts to components

--- resources/views/calls/index.blade.php
+++ resources/views/calls/index.blade.php
@@ -1,23 +1,7 @@
 @extends('layouts.app',['sum'=>$sum])
 @section('content')
 
-@if(Session::has('success'))
-    <div class="alert alert-success alert-dismissible fade show" role="alert">
-        {{ Session::get('success') }}
-        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-            <span aria-hidden="true">&times;</span>
-        </button>
-    </div>
-
-@elseif(Session::has('error'))
-    <div class="alert alert-danger alert-dismissible fade show" role="alert">
-        {{ Session::get('error') }}
-        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-            <span aria-hidden="true">&times;</span>
-        </button>
-    </div>
-@endif
-
+@include('components.alerts')
 @foreach($weekly_calls as $calls_per_day)
     {{-- Header --}}
     <div class="row md-row-reverse">

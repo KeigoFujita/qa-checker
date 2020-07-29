002b7951c5cbd30d3294055037c74b2e3306755c
Keigo Victor Fujita <keigofujita19@gmail.com>
2020-07-29 19:08:42 +0800

    Changed the libraries used to CDN

--- public/js/test.js
+++ public/js/test.js
@@ -1,43 +1,3 @@
-$('#editModal').on('show.bs.modal', function (event) {
-    var button = $(event.relatedTarget);
-    var company_id = button.data('id');
-    var company_name = button.data('name');
-    var modal = $(this);
-    modal.find('input[name=name]').val(company_name);
-    modal.find('input[name=company_id]').val(company_id);
-});
-
-
-$('#editCallModal').on('show.bs.modal', function (event) {
-    var button = $(event.relatedTarget);
-    var id = button.data('id');
-    var company_id = button.data('company-id');
-    var rating = button.data('rating');
-    var duration = button.data('duration');
-    var amount_earned = button.data('amount-earned');
-
-    var mins = Math.floor(duration / 60);
-    var seconds = duration % 60;
-
-    var modal = $(this);
-
-    modal.find('input[name=id]').val(id);
-    modal.find('select[name=company_id]').val(company_id);
-    modal.find('input[name=rating]').val(rating);
-    modal.find('#minutes').val(mins);
-    modal.find('#seconds').val(seconds);
-    modal.find('input[name=amount_earned]').val(amount_earned);
-});
-
-$('#deleteModal').on('show.bs.modal', function (event) {
-    var button = $(event.relatedTarget);
-    var company_id = button.data('id');
-    var company_name = button.data('name');
-    var modal = $(this);
-    modal.find('#company_name').text(company_name);
-    modal.find('input[name=company_id]').val(company_id);
-});
-
 $('#hard-refresh').click(function (event) {
     $(this).hide();
     $('#loading').show();
@@ -48,7 +8,7 @@ $('#hard-refresh').click(function (event) {
             $(this).show();
             location.reload();
         },
-        error:function(data){
+        error: function (data) {
             location.reload();
         }
     });
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,7 +9,9 @@
 
     <!-- Fonts -->
     <link href="" rel="stylesheet">
-    <link rel="stylesheet" href="{{ asset('css/_libraries.css') }}">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+    {{-- <link rel="stylesheet" href="{{ asset('css/_libraries.css') }}">
+    --}}
     <link rel="stylesheet" href="{{ asset('css/styles.css') }}">
 </head>
 
@@ -44,7 +46,13 @@
         </div>
     </main>
     @yield('modal')
-    <script src="{{ asset('js/app.js') }}"></script>
+
+    {{-- Bootstrap JS Bundles --}}
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+
+    {{-- Custom Script --}}
     <script src="{{ asset('js/test.js') }}"></script>
 </body>
 

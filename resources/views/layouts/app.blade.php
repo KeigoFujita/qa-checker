e4c8f04cc88d91fb4e435bc2b43740ba87f00e41
Keigo Victor Fujita <keigofujita19@gmail.com>
2020-07-29 18:59:09 +0800

    Latest stable release

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,12 +8,8 @@
     <title>QA Checker</title>
 
     <!-- Fonts -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
-        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-
-    {{-- Compiled version of Stylesheets --}}
-    {{-- <link rel="stylesheet" href="{{ asset('css/_libraries.css') }}">
-    --}}
+    <link href="" rel="stylesheet">
+    <link rel="stylesheet" href="{{ asset('css/_libraries.css') }}">
     <link rel="stylesheet" href="{{ asset('css/styles.css') }}">
 </head>
 
@@ -48,25 +44,8 @@
         </div>
     </main>
     @yield('modal')
-
-
-    {{-- JQuery CDN --}}
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
-        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous">
-    </script>
-
-    {{-- Popper JS --}}
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
-        integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous">
-    </script>
-
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
-        integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous">
-    </script>
-
-    {{-- Compiled version of the Javascript --}}
-    {{-- <script src="{{ asset('js/test.js') }}"></script> --}}
-    {{-- <script src="{{ asset('js/app.js') }}"></script> --}}
+    <script src="{{ asset('js/app.js') }}"></script>
+    <script src="{{ asset('js/test.js') }}"></script>
 </body>
 
 </html>
